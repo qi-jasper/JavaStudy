141c291ffae81b60a26fc01314e92b64a261a0d8
qi <qi.jasper@qq.com>
2020-11-01 22:43:35 +0800

    sum=sum+i

--- Basic-03-Thread/src/main/java/create/CreateByCallable.java
+++ Basic-03-Thread/src/main/java/create/CreateByCallable.java
@@ -28,7 +28,7 @@ class NumThread implements Callable {
         for(int i=1; i<=100; i++) {
             if(i%2 == 0) {
                 System.out.println(i);
-                sum += i;
+                sum = sum + i;
             }
         }
 
