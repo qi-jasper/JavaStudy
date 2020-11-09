5023da2d44ae0507140d47769edebc112b0f8a51
qi <qi.jasper@qq.com>
2020-11-09 15:44:35 +0800

    完善class读取文件部分

--- Basic-08-Reflect/src/main/java/classloaderdemo/LoadPropertiesTest.java
+++ Basic-08-Reflect/src/main/java/classloaderdemo/LoadPropertiesTest.java
@@ -3,8 +3,6 @@ package classloaderdemo;
 import org.junit.Test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
@@ -17,7 +15,7 @@ import java.util.Properties;
 public class LoadPropertiesTest {
 
     /**
-     * 方式一：使用 propertis 加载配置文件
+     * 方式一：使用 Propertis + FileInputStream 加载配置文件
      * @throws Exception
      */
     @Test
@@ -33,11 +31,42 @@ public class LoadPropertiesTest {
     }
 
     /**
-     * 方式二：配置文件默认识别为当前 module 的 src 下
+     * 方式二：使用 Class.getClass().getResource("/")
+     *        - 如果以 / 开头，默认从根路径开始搜索资源，即 `项目名称/target/classes`
+     *        - 如果不以 / 开头，默认从当前路径开始搜索资源，即从此类所在的包下取资源
+     *        - this.getClass().getResourceAsStream(): 直接将要读取的文件转化为流的形式了，相当于做了 `FileInputStream()`
+     *        - `this.getClass().getResource("/test.properties").getPath()` 和 `this.getClass().getResource("/test.properties").getFile()` 作用一致
+     */
+    @Test
+    public void loadProperties2() {
+        String path0 = LoadPropertiesTest.class.getClass().getResource("/").getPath();
+        // /C:/Users/qi/Documents/Code/Git/MyRepo/Java/JavaStudy/Basic-08-Reflect/target/classes/
+        System.out.println(path0);
+
+        String path = LoadPropertiesTest.class.getClass().getResource("/test.properties").getPath();
+        // /C:/Users/qi/Documents/Code/Git/MyRepo/Java/JavaStudy/Basic-08-Reflect/target/classes/test.properties
+        System.out.println(path);
+
+        String path1 = this.getClass().getResource("/test.properties").getPath();
+        // String path1 = this.getClass().getResource("/test.properties").getPath();
+        // /C:/Users/qi/Documents/Code/Git/MyRepo/Java/JavaStudy/Basic-08-Reflect/target/classes/test.properties
+        System.out.println(path1);
+
+        String path2 = this.getClass().getResource("LoadPropertiesTest.class").getPath();
+        // /C:/Users/qi/Documents/Code/Git/MyRepo/Java/JavaStudy/Basic-08-Reflect/target/classes/classloaderdemo/LoadPropertiesTest.class
+        System.out.println(path2);
+
+    }
+
+    /**
+     * 方式三：Class.getClassLoader().getResource(): 的资源获取不能以 / 开头，统一从根路径开始搜索资源
+     * 配置文件默认识别为当前 module 的 src 下
+     * Class.getClassLoader().getResourceAsStream(): 直接将要读取的文件转化为流的形式了，相当于做了 FileInputStream()
+     *
      * @throws Exception
      */
     @Test
-    public void loadProperties2() throws Exception {
+    public void loadProperties3() throws Exception {
         Properties properties = new Properties();
         ClassLoader classLoader = LoadPropertiesTest.class.getClassLoader();
 
@@ -45,6 +74,9 @@ public class LoadPropertiesTest {
         InputStream resourceAsStream1 = classLoader.getResourceAsStream("src\\main\\resources\\test.properties");
         // 这条正确
         InputStream resourceAsStream = classLoader.getResourceAsStream("test.properties");
+
+        // /C:/Users/qi/Documents/Code/Git/MyRepo/Java/JavaStudy/Basic-08-Reflect/target/classes/test.properties
+        System.out.println(classLoader.getResource("test.properties").getPath());
 
         properties.load(resourceAsStream);
 
