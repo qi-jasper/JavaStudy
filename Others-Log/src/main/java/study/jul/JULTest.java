57b2706cdbf62d554881dc8af70f5bf735a38b1a
qi <qi.jasper@qq.com>
2021-03-23 17:44:23 +0800

    feat[log]:JUL和Log4j学习

---
+++ Others-Log/src/main/java/study/jul/JULTest.java
@@ -0,0 +1,122 @@
+package study.jul;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.*;
+
+/**
+ * @Description
+ * @Author qi
+ * @Date 2021/3/21 20:21
+ * @ClassName JULTest
+ **/
+public class JULTest {
+
+    /**
+     * 入门 Demo
+     */
+    @Test
+    public void quickTest() {
+        // 1.获取日志记录器对象
+        Logger logger = Logger.getLogger("JULTest");
+        // 2.日志记录输出
+        logger.info("JUL Test");
+        
+        // 通用方法进行日志记录
+        logger.log(Level.INFO, "info message");
+
+        // 通过占位符的方式输出变量值
+        String name = "qi";
+        Integer age = 23;
+        logger.log(Level.INFO, "用户信息：姓名：{0}，年龄：{1}", new Object[]{name, age});
+    }
+
+    /**
+     * 日志等级
+     */
+    @Test
+    public void logLevelTest() {
+        // 1.获取日志记录器对象
+        Logger logger = Logger.getLogger("logLevelTest");
+        // 2.日志记录输出
+        logger.severe("SERVERE");
+        logger.warning("WARNING LEVEL");
+        // JUL 默认日志级别是 INFO，其下面的日志信息不会被输出
+        logger.info("INFO LEVEL");
+        logger.config("CONFIG LEVEL");
+        // 一般 debug 时使用该级别
+        logger.fine("FINE LEVE");
+    }
+
+    /**
+     * 自定义日志等级
+     */
+    @Test
+    public void definedLogLevel() {
+        // 1.获取日志记录器对象
+        Logger logger = Logger.getLogger("definedLogLevel");
+
+        // 2.关闭系统默认配置
+        logger.setUseParentHandlers(false);
+
+        // 3.自定义配置日志级别
+        // 3-1.创建 ConsolHandler，这是控制台输出处理，还可以创建 FileHandler 文件输出处理
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        // FileHandler fileHandler = new FileHandler("/logs/jul.log");
+
+        // 3-2.创建简单格式转换对象
+        SimpleFormatter simpleFormatter = new SimpleFormatter();
+
+        // 4.进行关联
+        consoleHandler.setFormatter(simpleFormatter);
+        // fileHandler.setFormatter(simpleFormatter);
+        logger.addHandler(consoleHandler);
+        // logger.addHandler(fileHandler);
+
+        // 5.配置日志具体级别
+        logger.setLevel(Level.ALL);
+        consoleHandler.setLevel(Level.ALL);
+
+        logger.severe("SERVERE");
+        logger.warning("WARNING LEVEL");
+        logger.info("INFO LEVEL");
+        logger.config("CONFIG LEVEL");
+        logger.fine("FINE LEVE");
+    }
+
+    /**
+     * Logger 对象父子关系
+     */
+    @Test
+    public void loggerRelationship() {
+        // study 下的 jul 包
+        Logger logger1 = Logger.getLogger("study.jul");
+        // study 包
+        Logger logger2 = Logger.getLogger("study");
+
+        // logger2 是否是 logger1 的父对象:true
+        System.out.println(logger1.getParent() == logger2);
+        // logger2 的父对象：java.util.logging.LogManager$RootLogger, 它是所有日志记录器的顶级父元素，其名称为：(空)
+        System.out.println("logger2 的父对象：" + logger2.getParent() + ", 它是所有日志记录器的顶级父元素，其名称为：" + logger2.getParent().getName());
+    }
+
+    /**
+     * 加载自定义的日志配置文件
+     */
+    @Test
+    public void definedLogPropertiesTest() throws IOException {
+        // 通过类加载器读取配置文件
+        InputStream ins = JULTest.class.getClassLoader().getResourceAsStream("logging.properties");
+        // 创建 LogManager
+        LogManager logManager = LogManager.getLogManager();
+        // 通过 LogManager 加载配置文件
+        logManager.readConfiguration(ins);
+
+        // 创建日志记录器
+        Logger logger = Logger.getLogger("definedLogPropertiesTest");
+
+        logger.info("INFO LEVEL");
+    }
+}
---
+++ Others-Log/src/main/java/study/log4j/Log4jTest.java
@@ -0,0 +1,52 @@
+package study.log4j;
+
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * @Description
+ * @Author qi
+ * @Date 2021/3/23 9:46
+ * @ClassName Log4jTest
+ **/
+public class Log4jTest {
+
+    @Test
+    public void quickStartTest() {
+
+        // 开启 Log4j 内置日志记录
+//        LogLog.setInternalDebugging(true);
+
+        // 因为没有自定义文件，在入门案例中暂不使用配置文件，所以这里初始化配置信息
+//        BasicConfigurator.configure();
+        // 创建日志记录器对象
+        Logger logger = Logger.getLogger(Log4jTest.class);
+        // 日志记录输出
+        logger.info("Quick Start");
+
+        // 日志级别
+        // 严重错误：表示严重错误，一般会造成系统崩溃
+        logger.fatal("fatal");
+
+        // 错误信息，不会影响系统运行
+        logger.error("error");
+        // 警告信息，可能会发生的问题
+        logger.warn("warning");
+        // 运行信息，数据库连接，网络连接，IO 操作等
+        logger.info("info");
+        // 调试信息，一般在开发中使用，记录程序变量参数传递信息等等。这是 log4j 默认级别
+        logger.debug("debug");
+
+        // 追踪信息，记录程序所有的流程信息。这是 log4j 最低级别
+        logger.trace("trance");
+    }
+
+    @Test
+    public void definedLoggerTest() {
+        // 创建日志记录器对象
+        Logger logger = Logger.getLogger(Log4jTest.class);
+        logger.info("definedLogger");
+    }
+
+}
